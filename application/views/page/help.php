441c7f7c7ac8747396f98245c9e4cde13786feef
Ensky <enskylin@gmail.com>
2014-05-13 01:02:09 +0800

    update the help

--- application/views/page/help.php
+++ application/views/page/help.php
@@ -1,3 +1,9 @@
 <h1>Help</h1>
-<p>This is help text</p>
+<ul>
+    <li>The test will be closed book, open reference(reference only, no any other website)</li>
+    <li>Refernce: <a target="_blank" href="http://dev.mysql.com/doc/refman/5.6/en/functions.html">http://dev.mysql.com/doc/refman/5.6/en/functions.html</a></li>
+    <li>Refernce: <a target="_blank" href="http://docs.oracle.com/cd/E17952_01/refman-5.6-en/refman-5.6-en.pdf">http://docs.oracle.com/cd/E17952_01/refman-5.6-en/refman-5.6-en.pdf</a></li>
+    <li>Refernce: <a target="_blank" href="http://docstore.mik.ua/orelly/linux/sql/ch15_01.htm">http://docstore.mik.ua/orelly/linux/sql/ch15_01.htm</a></li>
+    <li>Reference: you can see <a target="_blank" href="http://e3.nctu.edu.tw">e3</a> for the course slides</li>
+</ul>
 <a href="<?= site_url('main') ?>" class="btn btn-primary">I Accept</a>
