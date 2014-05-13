3c5399fb3d06f066874d0ba6e27c4ee51bfc6577
ensky <enskylin@gmail.com>
2014-05-13 22:45:47 +0800

    help

--- application/views/page/help.php
+++ application/views/page/help.php
@@ -1,7 +1,7 @@
 <h1>Help</h1>
 <ul>
     <li>This test is closed book, open reference(reference only, no any other website)</li>
-    <li>Reference: <a target="_blank" href="http://db2014.ensky.tw/mysql-refman-5.5-en.chm">MySQL E-Book(highly recommanded!)</a></li>
+    <li>Reference: <a target="_blank" href="http://db2014.ensky.tw/mysql.zip">MySQL E-Book(highly recommanded!)</a></li>
     <li>Reference: <a target="_blank" href="http://db2014.ensky.tw/w3schools/sql/default.asp">W3Schools</a></li>
     <li>Reference: <a target="_blank" href="http://db2014.ensky.tw/slides.zip">Course slides</a></li>
 </ul>
